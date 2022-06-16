fe8ccf99e9bed7f4d319e47595426a343f3fc77b
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-06-16 16:51:26 +0530

    Custom array adapter created commit

---
+++ app/src/main/java/com/example/gapshap/MessageAdapter.java
@@ -0,0 +1,51 @@
+package com.example.gapshap;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.squareup.picasso.Picasso;
+
+public class MessageAdapter extends ArrayAdapter<ModelClass> {
+
+    private Context context;
+
+
+    public MessageAdapter(@NonNull Context context, int resource, @NonNull ModelClass[] objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent, false);
+        }
+
+        ImageView photoImageView = (ImageView) convertView.findViewById(R.id.photoImageView);
+        TextView messageTextView = (TextView) convertView.findViewById(R.id.messageTextView);
+        TextView authorTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+
+        ModelClass message = getItem(position);
+
+        boolean isPhoto = message.getPhotoUrl() != null;
+        if (isPhoto) {
+            messageTextView.setVisibility(View.GONE);
+            photoImageView.setVisibility(View.VISIBLE);
+            Picasso.with(context).load(message.getPhotoUrl()).fit().centerInside().into(photoImageView);
+        } else {
+            messageTextView.setVisibility(View.VISIBLE);
+            photoImageView.setVisibility(View.GONE);
+            messageTextView.setText(message.getText());
+        }
+        authorTextView.setText(message.getName());
+
+        return convertView;
+    }
+
+}
